e9aae8352fed630875115dd189bdeb38255bf708
Aaron Curtis <aaron.curtis@jpl.nasa.gov>
2023-05-20 19:25:54 -0700

    add new items to display

--- src/screenTest.py
+++ src/screenTest.py
@@ -6,7 +6,9 @@ from time import sleep
 
 # Make a display
 epd = epd2in13_V3.EPD()
-font = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 40)
+font_big = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 35)
+font_medium = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 20)
+font_small = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 15)
 logging.info("init and Clear")
 # partial update
 canvas = Image.new("1", (epd.width, epd.height), 255)
@@ -19,8 +21,20 @@ epd.displayPartBaseImage(epd.getbuffer(canvas))
 
 data={'distance':0,'confidence':100}
 for x in arange(0, 20, 0.1):
-    data_str = f"{x :4.1f}m\n{data['confidence']:4}%"
     draw.rectangle((0, 0, epd.width, epd.height), fill=255)
-    draw.text((0, 0), data_str, font=font, fill=0)
+
+    draw.text((0, -5), "depth", font=font_small)
+    draw.text((0, 10), f"{x :4.1f}m", font=font_big, fill=0)
+
+    draw.text((0, 45), "confidence", font=font_small)
+    draw.text((0, 60), f"{data['confidence']:4}%", font=font_big, fill=0)
+
+    draw.text((0, 95), "battery", font=font_small)
+    draw.text((0, 110), " 33%", font=font_big, fill=0)
+
+    draw.text((0, 145), "gps", font=font_small)
+
+    draw.text((0, 160), "6 fix\n11S 444699\n   3696019", font=font_medium)
+
     epd.displayPartial(epd.getbuffer(canvas))
     sleep(1)
--- src/sonarDisp.py
+++ src/sonarDisp.py
@@ -20,15 +20,17 @@ myPing.initialize()
 
 # Make a display
 epd = epd2in13_V3.EPD()
-font = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 40)
+font_big = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 35)
+font_medium = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 20)
+font_small = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 15)
 logging.info("init and Clear")
 
-time_image = Image.new("1", (epd.width, epd.height), 255)
-time_draw = ImageDraw.Draw(time_image)
+canvas = Image.new("1", (epd.width, epd.height), 255)
+draw = ImageDraw.Draw(canvas)
 
 epd.init()
 epd.Clear(0xFF)
-epd.displayPartBaseImage(epd.getbuffer(time_image))
+epd.displayPartBaseImage(epd.getbuffer(canvas))
 
 with open(f'/home/aaron/data/{datetime.now()}.txt','w') as profiledata:
     while True:
@@ -36,8 +38,19 @@ with open(f'/home/aaron/data/{datetime.now()}.txt','w') as profiledata:
         profile = myPing.get_profile()
         profile['timestamp'] = datetime.now().isoformat().replace(':','-')
         profiledata.write(str(profile)+'\n')
-        data_str = f"{data['distance'] / 1000 :4.1f}m\n{data['confidence']:3}%"
-        logging.info(data_str)
-        time_draw.rectangle((0, 0, epd.width, epd.height), fill=255)
-        time_draw.text((0, 0), data_str, font=font, fill=0)
-        epd.displayPartial(epd.getbuffer(time_image))
+        draw.rectangle((0, 0, epd.width, epd.height), fill=255)
+
+        draw.text((0, -5), "depth", font=font_small)
+        draw.text((0, 10), f"{data['distance'] / 1000 :4.1f}m", font=font_big, fill=0)
+
+        draw.text((0, 45), "confidence", font=font_small)
+        draw.text((0, 60), f"{data['confidence']:3}%", font=font_big, fill=0)
+
+        draw.text((0, 95), "battery", font=font_small)
+        draw.text((0, 110), " 33%", font=font_big, fill=0)
+
+        draw.text((0, 145), "gps", font=font_small)
+
+        draw.text((0, 160), "6 fix\n11S 444699\n   3696019", font=font_medium)
+
+        epd.displayPartial(epd.getbuffer(canvas))
