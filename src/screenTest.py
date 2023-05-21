8093f473bec1b94969718efde1f49d915686957c
Aaron Curtis <aaron.curtis@jpl.nasa.gov>
2023-05-20 18:54:46 -0700

    update for v3

--- src/screenTest.py
+++ src/screenTest.py
@@ -1,24 +1,26 @@
 import logging
 from numpy import arange
-from waveshare_epd import epd2in13_V2
+from waveshare_epd import epd2in13_V3
 from PIL import Image, ImageDraw, ImageFont
-
+from time import sleep
 
 # Make a display
-epd = epd2in13_V2.EPD()
-font = ImageFont.truetype("/home/pi/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 40)
+epd = epd2in13_V3.EPD()
+font = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 40)
 logging.info("init and Clear")
 # partial update
-time_image = Image.new("1", (epd.width, epd.height), 255)
-time_draw = ImageDraw.Draw(time_image)
+canvas = Image.new("1", (epd.width, epd.height), 255)
+canvas.transpose(Image.ROTATE_180)
+draw = ImageDraw.Draw(canvas)
 
-epd.init(epd.FULL_UPDATE)
-epd.displayPartBaseImage(epd.getbuffer(time_image))
-epd.init(epd.PART_UPDATE)
+epd.init()
+epd.Clear(255)
+epd.displayPartBaseImage(epd.getbuffer(canvas))
 
 data={'distance':0,'confidence':100}
 for x in arange(0, 20, 0.1):
     data_str = f"{x :4.1f}m\n{data['confidence']:4}%"
-    time_draw.rectangle((0, 0, epd.width, epd.height), fill=255)
-    time_draw.text((0, 0), data_str, font=font, fill=0)
-    epd.displayPartial(epd.getbuffer(time_image))
+    draw.rectangle((0, 0, epd.width, epd.height), fill=255)
+    draw.text((0, 0), data_str, font=font, fill=0)
+    epd.displayPartial(epd.getbuffer(canvas))
+    sleep(1)
--- src/sonarDisp.py
+++ src/sonarDisp.py
@@ -2,15 +2,14 @@
 # -*- coding:utf-8 -*-
 # display stuff
 import logging
-from waveshare_epd import epd2in13_V2
+from waveshare_epd import epd2in13_V3
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
-from time import sleep
 
 # sonar stuff
 from brping import Ping1D
 
-logging.basicConfig(level=logging.DEBUG, filename="/home/pi/sonarlogs.txt")
+logging.basicConfig(level=logging.DEBUG, filename="/home/aaron/sonarlogs.txt")
 
 
 # Make a new sonar
@@ -20,21 +19,18 @@ myPing.connect_serial("/dev/ttyUSB0", 115200)
 myPing.initialize()
 
 # Make a display
-epd = epd2in13_V2.EPD()
-font = ImageFont.truetype("/home/pi/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 40)
+epd = epd2in13_V3.EPD()
+font = ImageFont.truetype("/home/aaron/surfsonar/src/ConsolaMonoBold-A9Am.ttf", 40)
 logging.info("init and Clear")
 
-outp_image = Image.new("1", (epd.width, epd.height), 255)
-outp_draw = ImageDraw.Draw(outp_image)
+time_image = Image.new("1", (epd.width, epd.height), 255)
+time_draw = ImageDraw.Draw(time_image)
 
-epd.init(epd.FULL_UPDATE)
-epd.displayPartBaseImage(epd.getbuffer(outp_image))
-epd.init(epd.PART_UPDATE)
+epd.init()
+epd.Clear(0xFF)
+epd.displayPartBaseImage(epd.getbuffer(time_image))
 
-#Color top half, where ghosting is, black
-outp_draw.rectangle((0, 0, epd.width, epd.height / 2), fill=0)
-
-with open(f'/home/pi/data/{datetime.now()}.txt','w') as profiledata:
+with open(f'/home/aaron/data/{datetime.now()}.txt','w') as profiledata:
     while True:
         data = myPing.get_distance()
         profile = myPing.get_profile()
@@ -42,7 +38,6 @@ with open(f'/home/pi/data/{datetime.now()}.txt','w') as profiledata:
         profiledata.write(str(profile)+'\n')
         data_str = f"{data['distance'] / 1000 :4.1f}m\n{data['confidence']:3}%"
         logging.info(data_str)
-        outp_draw.rectangle((0, 0, epd.width, epd.height), fill=255)
-        outp_draw.text((0, epd.height / 2), data_str, font=font, fill=0)
-        epd.displayPartial(epd.getbuffer(outp_image))
-        sleep(1)
+        time_draw.rectangle((0, 0, epd.width, epd.height), fill=255)
+        time_draw.text((0, 0), data_str, font=font, fill=0)
+        epd.displayPartial(epd.getbuffer(time_image))
